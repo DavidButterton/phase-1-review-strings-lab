57ddf1a1a23498f14fd4d85e0eca9c1e364ec2d4
DavidButterton <sadhu.butterton@gmail.com>
2023-10-28 13:07:44 -0700

    Phase 1 review Strings lab complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = "DAVID BUTTERTON!"
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}`;
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
